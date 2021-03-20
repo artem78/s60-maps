64e0f3b3988b06b1a4ce422764412d823faf3ecf
artem <megabyte1024@ya.ru>
2021-03-20 17:47:20 +0300

    Remake settings saving

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -9,6 +9,7 @@
  */
 
 #include "Settings.h"
+#include "Logger.h"
 
 
 // Constants
@@ -60,18 +61,28 @@ void CSettings::ExternalizeL(RWriteStream& aStream) const
 	aStream << iLon;
 	aStream << TCardinality(iZoom);
 	aStream << iTileProviderId;
-	aStream << (TInt8) iFullScreen; // No bool to stream conversion
+	aStream << (TInt8) iFullScreen; // No buil in bool to stream conversion
+	}
+
+void CSettings::DoInternalizeL(RReadStream& aStream)
+	{
+	TCardinality zoom;
+	TInt8 fullScreen;
+	
+	aStream >> iLat;
+	aStream >> iLon;
+	aStream >> zoom;
+	iZoom = (TInt) zoom;
+	aStream >> iTileProviderId;
+	aStream >> fullScreen;
+	iFullScreen = (TBool) fullScreen;
 	}
 
 void CSettings::InternalizeL(RReadStream& aStream)
 	{
-	TRAP_IGNORE(aStream >> iLat);
-	TRAP_IGNORE(aStream >> iLon);
-	TCardinality zoom;
-	TRAP_IGNORE(aStream >> zoom);
-	iZoom = (TInt) zoom;
-	TRAP_IGNORE(aStream >> iTileProviderId);
-	TInt8 fullScreen = 0;
-	TRAP_IGNORE(aStream >> fullScreen);
-	iFullScreen = fullScreen;
+	TRAPD(r, DoInternalizeL(aStream)); // Ignore any errors
+	if (r != KErrNone)
+		{
+		WARNING(_L("Settings file broken or incomplete (code: %d)"), r);
+		}
 	}
