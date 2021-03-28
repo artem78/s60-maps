29e35c9e9832fd05ad7553ca2620ba9e29b3aa17
artem <megabyte1024@ya.ru>
2021-03-28 20:00:42 +0300

    Remove stub and make implementation of recieving iaps list

--- src/IapUtils.cpp
+++ src/IapUtils.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "IapUtils.h"
+#include <metadatabase.h> // CMDBSession, CMDBRecordSet
+#include <commsdattypesv1_1.h> // CCDIAPRecord, KCDTIdIAPRecord
 
 // Constants
 const TInt KDefaultIapArrayGranularity = 5 /*10*/;
@@ -13,19 +15,41 @@ const TInt KDefaultIapArrayGranularity = 5 /*10*/;
 
 void IapUtils::GetAllIapsL(CIapArray* anArray)
 	{
+	using namespace CommsDat;
+	
 	anArray->Reset();
 	
-	////// STUB //////
-	// ToDo: Replace by real code
-	for (TInt i = 1; i <= 5; i++)
+	// Open the comms repository
+	CMDBSession* db = CMDBSession::NewLC(KCDVersion1_1 /*KCDCurrentVersion*/);
+	// Create a recordset (table) of IAPs
+	CMDBRecordSet<CCDIAPRecord>* iapRecordSet =
+			new (ELeave) CMDBRecordSet<CCDIAPRecord>(KCDTIdIAPRecord);
+	CleanupStack::PushL(iapRecordSet);
+	// Load the IAP recordset from the comms repository
+	iapRecordSet->LoadL(*db);
+	// Iterate through the recordset
+	CCDIAPRecord* iap = NULL;
+	for (TInt i = 0; i < iapRecordSet->iRecords.Count(); ++i)
 		{
+		// Get the IAP record (CCDIAPRecord)
+		iap = static_cast<CCDIAPRecord*>(iapRecordSet->iRecords[i]);
+		// We can now get information about the IAP.
+		
 		TIapItem item;
-		item.iId = i - 1;
-		item.iName.Copy(_L("IAP "));
-		item.iName.AppendNum(i);
+		item.iId = iap->RecordId();
+		
+		/////////////
+		_LIT(KFmt, "%S (id %d)");
+		TPtrC ptr(static_cast<const TDesC&>(iap->iRecordName));
+		item.iName.Format(KFmt, /*&iap->iRecordName*/ &ptr, iap->RecordId());
+		/////////////
+		
+		//item.iName.Copy(iap->iRecordName);
+		
 		anArray->AppendL(item);
 		}
-	//////////////////
+	// Close the IAP recordset and the database session
+	CleanupStack::PopAndDestroy(2, db);
 	}
 
 TBool IapUtils::IsIapAvailableL(TUint32 anIapId)
