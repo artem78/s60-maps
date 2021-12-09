3f4814cf622f24b43b4c6de4036a27b91dfa071f
artem <megabyte1024@ya.ru>
2021-12-09 18:46:20 +0300

    [Build] Make bash script for packing complete sources into ZIP archive

---
+++ MakeSourcesZip.sh
@@ -0,0 +1,12 @@
+mkdir -p sources
+cd sources
+git clone --recurse-submodules git@github.com:artem78/s60-maps.git
+cd s60-maps
+# Get latest annotated tag (release)
+TagName=$(git describe --abbrev=0)
+echo "Current program version: $TagName"
+git checkout $TagName
+rm -r -f .git
+zip ../s60-maps_sources_$TagName.zip . -r
+cd ..
+rm -r -f s60-maps
