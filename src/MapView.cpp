1e3c84abbc096443ddca3feb85bc2d7bc8a5dfd4
artem <megabyte1024@ya.ru>
2022-03-01 17:31:51 +0300

    [Fix] Application still follows phone's location after going to landmark/coordinate selected (issue #46)

--- src/MapView.cpp
+++ src/MapView.cpp
@@ -564,6 +564,7 @@ void CMapView::HandleGotoLandmarkL()
 		CPosLandmark* lm = appUi->LandmarkDb()->ReadLandmarkLC(lmIdArray->At(chosenItem));
 		TLocality pos;
 		lm->GetPosition(pos);
+		MapControl()->SetFollowUser(EFalse);
 		MapControl()->MoveAndZoomIn(pos);
 		CleanupStack::PopAndDestroy(lm);
 		}
@@ -580,6 +581,7 @@ void CMapView::HandleGotoCoordinateL()
 	if(dlg->ExecuteLD(R_LOCATION_QUERY_DIALOG) == EAknSoftkeyOk)
 		{
 		coord.SetCoordinate(pos.Latitude(), pos.Longitude());
+		MapControl()->SetFollowUser(EFalse);
 		MapControl()->MoveAndZoomIn(coord);
 		}
 	}
