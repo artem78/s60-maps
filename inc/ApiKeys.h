1d9b7e72ea7e76a5d68f1de72a784fb0d82afab1
Artem <megabyte1024@ya.ru>
2023-08-06 21:58:30 +0300

    Api keys memo

--- inc/ApiKeys.h
+++ inc/ApiKeys.h
@@ -10,6 +10,9 @@
 
 // API Keys
 // You can get your own here: https://manage.thunderforest.com/
+#ifndef _DEBUG
+#error Todo: Do not forget to set API keys for release build!
+#endif
 #ifdef _DEBUG
 _LIT8(KThunderForestApiKey, ""); // Development key
 #else
