8ae1a138470af10ae1324e1967f378f2770b742b
artem78 <megabyte1024@ya.ru>
2025-10-11 21:10:59 +0300

    edit drive availability check on start

--- src/S60MapsAppUi.cpp
+++ src/S60MapsAppUi.cpp
@@ -503,8 +503,10 @@ void CS60MapsAppUi::InternalizeL(RReadStream& aStream)
 		}
 	
 	// Check tile cache drive
-	if (!FileUtils::IsDriveWritable(iEikonEnv->FsSession(), iSettings->iTileCacheDrive))
-		{	
+	if (iSettings->iUseDiskCache && !FileUtils::IsDriveWritable(iEikonEnv->FsSession(), iSettings->iTileCacheDrive))
+		{
+		iSettings->iUseDiskCache = EFalse;
+		
 		HBufC* msgFmt = iEikonEnv->AllocReadResourceLC(R_TILE_CACHE_DRIVE_ERROR_FMT);
 		
 		RBuf msg;
@@ -526,9 +528,8 @@ void CS60MapsAppUi::InternalizeL(RReadStream& aStream)
 		CleanupStack::PopAndDestroy(2, msgFmt);
 		
 		
-		iSettings->iUseDiskCache = EFalse;
 		///////////////////////
-		iSettings->iTileCacheDrive = EDriveC;
+		//iSettings->iTileCacheDrive = EDriveC;
 		//////////////////////
 		}
 	
